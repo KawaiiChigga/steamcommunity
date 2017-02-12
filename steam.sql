98b18c85dc6bf411dda964ae5f247c4c1cb9a556
Daniel Gaunled <gaunled@gmail.com>
2017-02-12 20:30:29 +0700

    first

---
+++ steam.sql
@@ -0,0 +1,173 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 12, 2017 at 07:24 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `steam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `discussion`
+--
+
+CREATE TABLE `discussion` (
+  `discussionID` int(11) NOT NULL,
+  `gamename` varchar(50) NOT NULL,
+  `description` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friends`
+--
+
+CREATE TABLE `friends` (
+  `userID` int(11) NOT NULL,
+  `friendID` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `postID` int(11) NOT NULL,
+  `threadID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `message` varchar(512) NOT NULL,
+  `postDateTime` datetime NOT NULL,
+  `updateDateTime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thread`
+--
+
+CREATE TABLE `thread` (
+  `threadID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `discussionID` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `publishDateTime` datetime NOT NULL,
+  `isPinned` tinyint(4) NOT NULL,
+  `categoryType` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `imageURL` varchar(256) NOT NULL,
+  `description` varchar(256) DEFAULT NULL,
+  `name` varchar(100) NOT NULL,
+  `country` int(50) DEFAULT NULL,
+  `province` int(50) DEFAULT NULL,
+  `city` int(50) DEFAULT NULL,
+  `joinDate` datetime NOT NULL,
+  `discussionID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `discussion`
+--
+ALTER TABLE `discussion`
+  ADD PRIMARY KEY (`discussionID`);
+
+--
+-- Indexes for table `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`userID`,`friendID`),
+  ADD KEY `friendID_friends` (`friendID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`postID`),
+  ADD KEY `userID_post` (`userID`),
+  ADD KEY `threadID_post` (`threadID`);
+
+--
+-- Indexes for table `thread`
+--
+ALTER TABLE `thread`
+  ADD PRIMARY KEY (`threadID`),
+  ADD KEY `userID_thread` (`userID`),
+  ADD KEY `discussionID_thread` (`discussionID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`),
+  ADD KEY `discussionID_user` (`discussionID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `friends`
+--
+ALTER TABLE `friends`
+  ADD CONSTRAINT `friendID_friends` FOREIGN KEY (`friendID`) REFERENCES `user` (`userID`),
+  ADD CONSTRAINT `userID_friends` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `threadID_post` FOREIGN KEY (`threadID`) REFERENCES `thread` (`threadID`),
+  ADD CONSTRAINT `userID_post` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
+
+--
+-- Constraints for table `thread`
+--
+ALTER TABLE `thread`
+  ADD CONSTRAINT `discussionID_thread` FOREIGN KEY (`discussionID`) REFERENCES `discussion` (`discussionID`),
+  ADD CONSTRAINT `userID_thread` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `discussionID_user` FOREIGN KEY (`discussionID`) REFERENCES `discussion` (`discussionID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
